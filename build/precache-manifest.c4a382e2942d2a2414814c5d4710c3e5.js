8123b1971e3d0d23635d00bfd4e51a58a265e315
calderonjorge <jorgecalderaperalta@gmail.com>
2021-02-12 16:58:25 -0600

    build

---
+++ build/precache-manifest.c4a382e2942d2a2414814c5d4710c3e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b2f6ec76b9ea7a5322acf825ae5b92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4855f5973ed32bcab353",
+    "url": "/static/css/2.b0704b7a.chunk.css"
+  },
+  {
+    "revision": "f13472048174201055ee",
+    "url": "/static/css/main.c67b214b.chunk.css"
+  },
+  {
+    "revision": "4855f5973ed32bcab353",
+    "url": "/static/js/2.a379076d.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/static/js/2.a379076d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f13472048174201055ee",
+    "url": "/static/js/main.fd2debd3.chunk.js"
+  },
+  {
+    "revision": "b4e3307db6825ea9cb67",
+    "url": "/static/js/runtime-main.da0c3731.js"
+  }
+]);
